1a7a4505c3983dd4c061d83931f11474b2951c09
Suhas <suhas.gavas@gmail.com>
2026-02-07 22:54:34 +0530

    Update WebConfig.java

--- backend/src/main/java/com/example/springboot/config/WebConfig.java
+++ backend/src/main/java/com/example/springboot/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints in your API
-                .allowedOrigins("https://angular-springboot-enevx1aku-csmpers-projects.vercel.app") // Your Vercel URL
+                .allowedOrigins("https://angular-springboot-app.vercel.app") // Your Vercel URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Required if you are using Cookies or Sessions
